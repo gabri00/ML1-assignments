cbd3d3013d5d046ef517b75e112f523128432421
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2022-10-03 18:57:44 +0200

    add smoothing factor

---
+++ Naive_Bayes_classifier.py
@@ -0,0 +1,89 @@
+# Naive Bayes
+# 1. Read the data from the csv file
+# 2. Convert categorical data to numerical data
+# 3. Split the data into training and testing data
+# 4. Remove the target column from the training and testing data
+# 5. Build a Naive Bayes classifier
+# 6. Train the model
+# 7. Predict the output
+# 8. Print the output
+
+from turtle import pos
+import pandas as pd
+import numpy as np
+
+# Read the data from the csv file
+df = pd.read_csv('weather_dataset.csv', delim_whitespace=True)
+
+# Convert categorical data to numerical data
+for col in df.columns:
+    class_map = {label: idx for idx, label in enumerate(np.unique(df[col]))}
+    df[col] = df[col].map(class_map)
+
+# Split the data into training and testing data
+df1 = df.sample(frac=1)
+train_X = df1.head(10)
+test_X = df1.tail(4)
+train_y = train_X['Play']
+test_y = test_X['Play']
+
+# Remove the target column from the training and testing data
+train_X = train_X.drop('Play', axis=1)
+test_X = test_X.drop('Play', axis=1)
+
+# Add one to train_X and test_X to avoid zero probability
+train_X = train_X + 1
+test_X = test_X + 1
+
+# Build a Naive Bayes classifier
+class NaiveBayes:
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        self.classes = np.unique(y)
+        self.cond_prob = {}
+
+        # Calculate the prior probability of the target column
+        self.prior_prob = train_y.value_counts(normalize=True).to_dict()
+        
+        # Calculate the conditional probability of each feature
+        for _, c in enumerate(self.classes):
+            self.cond_prob[c] = {}
+            X_c = X[y==c]
+            for col in X_c.columns:
+                counts = X_c[col].value_counts()
+                self.cond_prob[c][col] = (counts / counts.sum()).to_dict()
+
+    def predict(self, X):
+        # Calculate the posterior probability for each pattern
+        posterior = {}
+        y = []
+        for row in range(len(X)):
+            posterior[row] = {}
+            for _, c in enumerate(self.classes):
+                posterior[row][c] = self.prior_prob[c]
+                for col in X.columns:
+                    try:
+                        posterior[row][c] *= self.cond_prob[c][col][X[col].iloc[row]]
+                    except KeyError:
+                        posterior[row][c] *= 0
+            y.append(self.classes[np.argmax(list(posterior[row].values()))])
+
+        return y
+
+    def score(self, y, y_pred):
+        return np.sum(y_pred == y) / len(y)
+
+# Train the model
+model = NaiveBayes()
+model.fit(train_X, train_y)
+
+# Predict the output
+y_pred = model.predict(test_X)
+
+# Print the output
+print('True output: ', ["yes" if val else "no" for val in test_y.values])
+print('Predicted output: ', ["yes" if val else "no" for val in y_pred])
+
+# Calculate the accuracy of the model
+print(f'Accuracy: {int(model.score(test_y, y_pred) * 100)}%')
--- Naive_Bayes_classifier_smooth.py
+++ Naive_Bayes_classifier_smooth.py
@@ -3,7 +3,7 @@
 # 2. Convert categorical data to numerical data
 # 3. Split the data into training and testing data
 # 4. Remove the target column from the training and testing data
-# 5. Build a Naive Bayes classifier
+# 5. Build a Naive Bayes classifier with smoothing
 # 6. Train the model
 # 7. Predict the output
 # 8. Print the output
@@ -24,14 +24,23 @@ for col in df.columns:
 df1 = df.sample(frac=1)
 train_X = df1.head(10)
 test_X = df1.tail(4)
-train_Y = train_X['Play']
-test_Y = test_X['Play']
+train_y = train_X['Play']
+test_y = test_X['Play']
 
 # Remove the target column from the training and testing data
 train_X = train_X.drop('Play', axis=1)
 test_X = test_X.drop('Play', axis=1)
 
-# Build a Naive Bayes classifier
+# Add one to train_X and test_X to avoid zero probability
+train_X = train_X + 1
+test_X = test_X + 1
+
+# Count unique values in each column
+unique_values = {}
+for col in df1.drop('Play', axis=1).columns:
+    unique_values[col] = df1[col].nunique()
+
+# Build a Naive Bayes classifier with smoothing
 class NaiveBayes:
     def fit(self, X, y):
         self.X = X
@@ -40,7 +49,7 @@ class NaiveBayes:
         self.cond_prob = {}
 
         # Calculate the prior probability of the target column
-        self.prior_prob = train_Y.value_counts(normalize=True).to_dict()
+        self.prior_prob = train_y.value_counts(normalize=True).to_dict()
         
         # Calculate the conditional probability of each feature
         for _, c in enumerate(self.classes):
@@ -49,38 +58,37 @@ class NaiveBayes:
             for col in X_c.columns:
                 counts = X_c[col].value_counts()
                 self.cond_prob[c][col] = (counts / counts.sum()).to_dict()
-
+                # self.cond_prob[c][col] = ((counts + 1) / (counts.sum() + unique_values[col])).to_dict()
 
     def predict(self, X):
         # Calculate the posterior probability for each pattern
         posterior = {}
         y = []
-        for row in range(len(X)):
-            posterior[row] = {}
+        for _, row in X.iterrows():
             for _, c in enumerate(self.classes):
-                posterior[row][c] = self.prior_prob[c]
-                for col in X.columns:
+                posterior[c] = self.prior_prob[c]
+                for col in row.index:
                     try:
-                        posterior[row][c] *= self.cond_prob[c][col][X[col].iloc[row]]
+                        posterior[c] *= self.cond_prob[c][col][row[col]]
                     except KeyError:
-                        posterior[row][c] *= 0
-            y.append(self.classes[np.argmax(list(posterior[row].values()))])
+                        posterior[c] *= 0
+            y.append(self.classes[np.argmax(list(posterior.values()))])
 
         return y
 
-    def score(self, y):
+    def score(self, y, y_pred):
         return np.sum(y_pred == y) / len(y)
 
 # Train the model
 model = NaiveBayes()
-model.fit(train_X, train_Y)
+model.fit(train_X, train_y)
 
 # Predict the output
 y_pred = model.predict(test_X)
 
 # Print the output
-print('True output: ', test_Y.values)
-print('Predicted output: ', y_pred)
+print('True output: ', ["yes" if val else "no" for val in test_y.values])
+print('Predicted output: ', ["yes" if val else "no" for val in y_pred])
 
 # Calculate the accuracy of the model
-print('Accuracy: ', model.score(y_pred))
+print(f'Accuracy: {int(model.score(test_y, y_pred) * 100)}%')
