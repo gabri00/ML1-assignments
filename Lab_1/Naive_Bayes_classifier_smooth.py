f1a40dc81763b61168f2a79008f08639d2f5265d
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2022-10-10 13:07:26 +0200

    lab 1 finished

--- Lab_1/Naive_Bayes_classifier_smooth.py
+++ Lab_1/Naive_Bayes_classifier_smooth.py
@@ -1,19 +1,16 @@
-# Naive Bayes
+# Naive Bayes (Laplace Smoothing)
 # 1. Read the data from the csv file
 # 2. Convert categorical data to numerical data
 # 3. Split the data into training and testing data
-# 4. Remove the target column from the training and testing data
-# 5. Build a Naive Bayes classifier with Laplace smoothing
-# 6. Train the model
-# 7. Predict the output
-# 8. Print the output
+# 4. Build a Naive Bayes classifier with Laplace smoothing
+# 5. Train the model
+# 6. Predict and print the output
 
 import pandas as pd
 import numpy as np
 
 # Set dataset path
 PATH = 'weather_dataset.csv'
-
 # Read the data from the csv file
 df = pd.read_csv(PATH, delim_whitespace=True)
 
@@ -34,13 +31,13 @@ def convert_categorical_to_numerical(df):
         df[col] = df[col].map(class_map)
 
 # Split the data into training and testing data
-def split_data(df, train_size=TRAIN_SIZE, test_size=TEST_SIZE):
+def split_data(df, train_size=TRAIN_SIZE, test_size=TEST_SIZE, targ_col=TARG_COL):
     df1 = df.sample(frac=1)
     train_X = df1.head(train_size)
     test_X = df1.tail(test_size)
-    test_y = test_X[TARG_COL]
+    test_y = test_X[targ_col]
     # Remove the target column from the testing data
-    test_X = test_X.drop(TARG_COL, axis=1)
+    test_X = test_X.drop(targ_col, axis=1)
     return train_X, test_X, test_y
 
 # Count unique values in each column
@@ -50,13 +47,12 @@ def count_unique_values(df):
         unique_values[col] = df[col].value_counts().to_dict()
     return unique_values
 
-# Build a Naive Bayes classifier with Laplace smoothing
+# Naive Bayes classifier with Laplace smoothing
 class NaiveBayes:
     def fit(self, X, unique_values, alpha=ALPHA):
         self.X = X
         self.y = self.X[TARG_COL]
         self.classes = np.unique(self.y)
-        self.unique_values = unique_values
         self.cond_prob = {}
 
         # Remove the target column from the training data
@@ -71,46 +67,48 @@ class NaiveBayes:
             X_c = self.X[self.y==c]
             for col in X_c.columns:
                 c_counts = X_c[col].value_counts()
-                for k in self.unique_values[col]:
+                for k in unique_values[col]:
                     if k not in c_counts:
-                        c_counts[k] = self.unique_values[col][k]
-
-                # Add smoothing
+                        c_counts[k] = 0
+                # Smoothing the conditional probability
                 self.cond_prob[c][col] = ((c_counts + alpha) / (X_c[col].value_counts().sum() + alpha*len(unique_values[col]))).to_dict()
 
     def predict(self, X):
-        # Calculate the posterior probability for each pattern
         posterior = {}
         y = []
+        # Calculate the posterior probability for each pattern
         for _, row in X.iterrows():
             for _, c in enumerate(self.classes):
                 posterior[c] = self.prior_prob[c]
                 for col in row.index:
                     posterior[c] *= self.cond_prob[c][col][row[col]]
-            
-            # for c in self.classes:
-            #     posterior[c] /= (posterior[self.classes[0]] + posterior[self.classes[1]])
-
             y.append(self.classes[np.argmax(list(posterior.values()))])
         return y
 
-    def score(self, y, y_pred):
-        return np.sum(y_pred == y) / len(y)
+    def error(self, y, y_pred):
+        # Return the error rate
+        return np.sum(y_pred != y) / len(y)
 
 
 convert_categorical_to_numerical(df)
 train_X, test_X, test_y = split_data(df)
-unique_values = count_unique_values(df)
 
 model = NaiveBayes()
 # Train the model
-model.fit(train_X, unique_values)
+model.fit(train_X, count_unique_values(df))
 # Predict the output
 y_pred = model.predict(test_X)
 
 # Print the output
-print('True output: ', ["yes" if val else "no" for val in test_y.values])
-print('Predicted output: ', ["yes" if val else "no" for val in y_pred])
+if (len(np.unique(df[TARG_COL])) == 1):
+    print('True output: ', ["yes" for _ in test_y.values])
+    print('Predicted output: ', ["yes" for _ in y_pred])
+elif (len(np.unique(df[TARG_COL])) == 2):
+    print('True output: ', ["yes" if val else "no" for val in test_y.values])
+    print('Predicted output: ', ["yes" if val else "no" for val in y_pred])
+else:
+    print('True output: ', [_ for _ in test_y.values])
+    print('Predicted output: ', y_pred)
 
 # Calculate and print the accuracy of the model
-print(f'Error: {int((1 - model.score(test_y, y_pred)) * 100)}%')
+print(f'Error: {int(model.error(test_y, y_pred) * 100)}%')
