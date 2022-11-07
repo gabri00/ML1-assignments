b7a30ebde575806b93195341914513e633f03062
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2022-11-07 12:13:26 +0100

    Update kNN.py

--- Lab_3/kNN.py
+++ Lab_3/kNN.py
@@ -1,32 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from collections import Counter
 from tensorflow.keras.datasets import mnist
+
+### Task 1 ###
 
 # Load mnist dataset
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 
-# Create functions for different distance metrics
-def euclidean_distance(x1, x2):
-   return np.sqrt(np.sum((x1 - x2)**2))
 
-def manhattan_distance(x1, x2):
-   return np.sum(np.abs(x1 - x2))
+### Task 2 ###
+
+# Function for Euclidean distance
+def euclidean_distance(x1, x2):
+   # return np.sqrt(np.sum((x1 - x2)**2))
+   return np.linalg.norm(x1 - x2)
 
 # Function to check the data
 def check_data(train_X, test_X, k):
-   # Check if the columns of the training and test data are the same
+   # Check if the number of columns in the training and test sets are the same
    if train_X.shape[1] != test_X.shape[1]:
-      raise ValueError("The number of columns in the training and test data is different!")
+      raise ValueError("The number of columns in the training and test sets is different!")
    # Check if k is in the correct range
    if k < 1 or k > train_X.shape[0]:
       raise ValueError("k is not in the correct range!")
 
-# Create function for kNN
+# Function for k-Nearst Neighbors algorithm
 def kNN(train_X, train_y, test_X, k, test_y=None):
    check_data(train_X, test_X, k)
 
-   # Calculate the distance between the test points and the training points
+   # Calculate the distances between the test images and the training images
    distances = np.zeros((test_X.shape[0], train_X.shape[0]))
    for i in range(test_X.shape[0]):
       for j in range(train_X.shape[0]):
@@ -40,35 +42,57 @@ def kNN(train_X, train_y, test_X, k, test_y=None):
    # Find the most common class in the k nearest neighbors
    y_pred = np.zeros(test_X.shape[0])
    for i in range(test_X.shape[0]):
-      y_pred[i] = Counter(train_y[k_neighbors[i].astype(int)]).most_common(1)[0][0]
+      y_pred[i] = np.argmax(np.bincount(train_y[k_neighbors[i].astype(int)]))
 
-   # Compute and return the error rate if test_y is given
+   # Compute and return the error rate if test_y is given otherwise return the predicted labels
    if test_y is not None:
-      return y_pred, np.sum(y_pred != test_y) / test_X.shape[0]
+      return np.sum(y_pred != test_y) / test_X.shape[0]
    else:
-      return y_pred
+      return y_pred.astype(int)
 
 
-# Select a random subset of the training data of size 1000
-idx = np.random.permutation(train_X.shape[0])[:100]
+# Select a random subset of the training data to reduce the computation time
+idx = np.random.permutation(train_X.shape[0])[:4000]
 train_X = train_X[idx]
 train_y = train_y[idx]
 
-# Predict each digit from the test set for every k and calculate the accuracy
-accuracies = {}
+# Classify 5% of the test data with k=5 (test_y is not given)
+idx = np.random.permutation(test_X.shape[0])[:int(round(test_X.shape[0] * 0.05))]
+
+prediction = kNN(train_X, train_y, test_X[idx], 5)
+print("Error rate: ", np.sum(prediction != test_y[idx]) / len(prediction))
+
+# Classify 5% of the test data with k=5 (test_y is given)
+error_rate = kNN(train_X, train_y, test_X[idx], 5, test_y[idx])
+print("Error rate: ", error_rate)
+
+
+### Task 3 ###
+
+classes = np.unique(test_y)
+accuracies = []
 k = [1, 2, 3, 4, 5, 10, 15, 20, 30, 40, 50]
 
-for i in range(10):
-   # Print the progress
-   test_X_i = test_X[test_y == i][:50]
-   test_y_i = test_y[test_y == i][:50]
-   accuracies[i] = []
-   for k_i in k:
-      accuracies[i].append((1 - kNN(train_X, train_y, test_X_i, k_i, test_y_i)[1])*100)
+# Predict each digit from the test set for every k and calculate the accuracy
+for i in range(len(classes)):
+   test_X_i = test_X[test_y == i]
+   test_y_i = test_y[test_y == i]
+   # Select a random 10% of test data to reduce the computation time
+   idx = np.random.permutation(test_X_i.shape[0])[:int(round(test_X_i.shape[0] * 0.1))]
+   test_X_i = test_X_i[idx]
+   test_y_i = test_y_i[idx]
 
-# Plot the error rate for each digit and each k
-for i in range(10):
+   accuracies.append([])
+   for k_i in k:
+      err_rate = kNN(train_X, train_y, test_X_i, k_i, test_y_i)
+      accuracies[i].append((1 - err_rate)*100)
+
+   # Plot the error rate for each digit and each k
    plt.plot(k, accuracies[i], label=f'Digit {i}')
+
+   # Print the progress
+   print("Progress: ", i + 1, "/ 10")
+
 plt.xlabel('k')
 plt.ylabel('Accuracy (%)')
 plt.legend()
