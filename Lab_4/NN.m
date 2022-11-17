35b78385f5af9e68414fbd95f2d21d11b97e6aa5
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2022-11-17 16:55:23 +0100

    init lab 4

--- Lab_4/NN.m
+++ Lab_4/NN.m
@@ -1,2 +1,9 @@
-predictors = [0 1 0 1; 0 0 1 1];
-responses = [0 0 0 1];
+wine_dataset = importdata('wine.data');
+wine_input = wine_dataset(:, 2:size(wine_dataset, 2))';
+wine_target = wine_dataset(:, 1)';
+
+classes = unique(wine_target);
+
+for i = 1:size(classes)
+    wine_target(i) = wine_target()
+
