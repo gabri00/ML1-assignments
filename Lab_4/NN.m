c5746b838f272fdba2e6f13084476a0a263ea2b4
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2022-11-23 15:56:54 +0100

    Update NN.m

--- Lab_4/NN.m
+++ Lab_4/NN.m
@@ -1,10 +1,14 @@
+% Configuration commands
 clear
 clc
-
 addpath('mnist\');
 addpath('plot\');
 
+% Subset percentage
 p = 0.05;
+% Number of neurons in the hidden layer
+hn = 2;
+% Classes to confront
 classes = [
     1, 8;
     3, 8;
@@ -13,6 +17,7 @@ classes = [
 ];
 
 for i = 1:size(classes, 1)
+    % Load sets
     [X_a, T_a] = loadMNIST(0, classes(i, 1));
     [X_b, T_b] = loadMNIST(0, classes(i, 2));
 
@@ -21,20 +26,21 @@ for i = 1:size(classes, 1)
     idx_a = randperm(size(X_a, 1), subset_size);
     idx_b = randperm(size(X_b, 1), subset_size);
 
+    % Create subset
     dataset = [
         X_a(idx_a, :);
         X_b(idx_b, :)
     ];
 
     % Train autoencoder
-    autoEncoder = trainAutoencoder(dataset', 2);
+    autoEncoder = trainAutoencoder(dataset', hn);
     encoded_data = encode(autoEncoder, dataset');
 
     % Plot data
     figure
     plotcl(encoded_data', [T_a(1:subset_size); T_b(1:subset_size)]);
-    legend(['Class ', num2str(T_a(1))], ['Class ', num2str(T_b(1))]);
+    legend(['Digit ', num2str(T_a(1))], ['Digit ', num2str(T_b(1))]);
     xlabel('Hidden unit 1');
     ylabel('Hidden unit 2');
-    title(['Autoencoder classes ', num2str(classes(i, 1)), ' and ', num2str(classes(i, 2))]);
+    title(['Autoencoder on digits ', num2str(classes(i, 1)), ' and ', num2str(classes(i, 2))]);
 end
