bb287a1166533ae79b82ae5cfd5d281d8ac989cc
gabri00 <gibby.nicchia@gmail.com>
2022-11-26 17:51:53 +0100

    Update NN.m

--- Lab_4/NN.m
+++ Lab_4/NN.m
@@ -1,4 +1,4 @@
-% Configuration commands
+% Configurations
 clear
 clc
 addpath('mnist\');
@@ -6,40 +6,37 @@ addpath('plot\');
 
 % Subset percentage
 p = 0.05;
-% Number of neurons in the hidden layer
-hn = 2;
+% Number of units in the hidden layer
+nh = 2;
 % Classes to confront
 classes = [
-    1, 8;
-    3, 8;
-    1, 7;
-    5, 6
+    1 8;
+    3 9;
+    1 7;
+    0 6;
+    2 5
 ];
 
-for i = 1:size(classes, 1)
-    % Load sets
-    [X_a, T_a] = loadMNIST(0, classes(i, 1));
-    [X_b, T_b] = loadMNIST(0, classes(i, 2));
+for i = 1:length(classes)
+    % Load data
+    [X, T] = loadMNIST(0, classes(i, :));
 
-    % Shuffle sets
-    subset_size = floor(size(X_a, 1)*p);
-    idx_a = randperm(size(X_a, 1), subset_size);
-    idx_b = randperm(size(X_b, 1), subset_size);
-
-    % Create subset
-    dataset = [
-        X_a(idx_a, :);
-        X_b(idx_b, :)
-    ];
+    % Srink dataset size, take only p% of the length
+    % Then shuffle the data
+    subset_size = floor(length(X)*p);
+    idx = randperm(length(X), subset_size);
+    X = X(idx, :);
+    T = T(idx, :);
 
     % Train autoencoder
-    autoEncoder = trainAutoencoder(dataset', hn);
-    encoded_data = encode(autoEncoder, dataset');
+    autoEncoder = trainAutoencoder(X', nh);
+    encoded_data = encode(autoEncoder, X');
 
     % Plot data
     figure
-    plotcl(encoded_data', [T_a(1:subset_size); T_b(1:subset_size)]);
-    legend(['Digit ', num2str(T_a(1))], ['Digit ', num2str(T_b(1))]);
+    plotcl(encoded_data', [T(T==classes(i, 1), :); T(T==classes(i, 2), :)]);
+
+    legend(['Digit ', num2str(classes(i, 1))], ['Digit ', num2str(classes(i, 2))]);
     xlabel('Hidden unit 1');
     ylabel('Hidden unit 2');
     title(['Autoencoder on digits ', num2str(classes(i, 1)), ' and ', num2str(classes(i, 2))]);
